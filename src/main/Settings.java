22739db75d85720d09c655ad466b83b0aa92cb14
MatthieuBlm <matthieu.bellamy@hotmail.fr>
2016-08-16 21:28:58 +0200

    Delete final modifier to allow modification to test different combination.

--- src/main/Settings.java
+++ src/main/Settings.java
@@ -5,24 +5,24 @@ import java.awt.Color;
 public abstract class Settings {
 	// Base
 	public static final String APPLICATION_TITLE 	= "Super Cell Simulator 3000";
-	public static final int WINDOW_SIZE_X 			= 800;
-	public static final int WINDOW_SIZE_Y 			= 800;
+	public static final int WINDOW_SIZE_X 			= 400;
+	public static final int WINDOW_SIZE_Y 			= 400;
 			
 	// Simulation
-	public static final double CELL_RATE 			= 0.2;				// Between 0 and 1, used for generate random universe
-	public static final int NB_CELL_WIDTH 			= 800;
-	public static final int NB_CELL_HEIGH 			= 800;
-	public static final long PARIOD_DURATION 		= 25;				// In milliseconds
+	public static double CELL_RATE 					= 0.7;				// Between 0 and 1, used for generate random universe
+	public static final int NB_CELL_WIDTH 			= 200;
+	public static final int NB_CELL_HEIGH 			= 200;
+	public static final long PARIOD_DURATION 		= 50;				// In milliseconds
 	public static final int UP 						= 0;
 	public static final int RIGHT					= 1;
 	public static final int DOWN					= 2;
 	public static final int LEFT					= 3;
 	
 	// 			Wator Simultation
-	public static final int FISH_REPRODUCTION_STEPS 	= 10;
-	public static final int SHARK_REPRODUCTION_STEPS 	= 50;
-	public static final int SHARK_ENERGIE				= 30;
-	public static final double CELL_BALANCE 			= 0.3;					// Balance between Sharks and Fish. 0 = Only fish, 1 = only shark
+	public static int FISH_REPRODUCTION_STEPS 	= 10;
+	public static int SHARK_REPRODUCTION_STEPS 	= 30;
+	public static int SHARK_ENERGIE				= 30;
+	public static double CELL_BALANCE 			= 0.3;					// Balance between Sharks and Fish. 0 = Only fish, 1 = only shark
 
 	// Display
 	public static final Color CELL_COLOR 			= Color.GRAY;
